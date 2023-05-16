64379eb4e738347abfd1a748775eba19319de0aa
Ewa Chojnacka <ewa.katarzyna.chojnacka@gmail.com>
2023-05-16 16:38:01 +0200

    Add some basic requests

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require('express')
 const app = express()
+
+let newAlerts = []
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+
+app.post('/alert', (req, res) => {
+    const newAlert = request.body;
+    console.log("Got a new alert", newAlert)
+    alerts.push(newAlert);
+    res.send("Got new alert, thanks!");
+});
+
+app.get('/new', (req, res) => {
+    const recentAlerts = newAlerts;
+    newAlerts = [];
+    res.send(recentAlerts)
+});
+
+
 app.listen(process.env.PORT || 3000)
